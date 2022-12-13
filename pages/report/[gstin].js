c32663f6965f81bb400e08efdf86322f8fc1b527
aadi58002 <aadi58002gmail.com>
2022-12-13 08:59:56 +0530

    Adding the gstr3b Table

--- pages/report/[gstin].js
+++ pages/report/[gstin].js
@@ -12,12 +12,17 @@ pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const { backendURL } = config;
 
-const DynamicGSTR9 = dynamic(() => import("../../components/gstr9"), {
-  ssr: false,
-});
+const DynamicGSTR9 = dynamic(() => import("../../components/gstr9"), {ssr: false});
+const DynamicGSTR3B = dynamic(() => import("../../components/gstr3b"), {ssr: false});
 
 async function getGSTR9Data(gstin, callback) {
   let res = await fetch(`${backendURL}/api/v1/r9?GSTIN=${gstin}`);
+  let data = await res.json();
+  callback(data.data);
+}
+
+async function getGSTR3BData(gstin, callback) {
+  let res = await fetch(`${backendURL}/api/v1/r3b?GSTIN=${gstin}`);
   let data = await res.json();
   callback(data.data);
 }
@@ -28,10 +33,12 @@ export default function GSTSummary() {
   const { gstin } = router.query;
   console.log({ gstin });
 
-  let [GSTR9Data, setGSTR9Data] = useState(null);
+  // let [GSTR9Data, setGSTR9Data] = useState(null);
+  let [GSTR3BData, setGSTR3BData] = useState(null);
   useEffect(() => {
     if (gstin) {
-      getGSTR9Data(gstin, setGSTR9Data);
+      // getGSTR9Data(gstin, setGSTR9Data);
+      getGSTR3BData(gstin, setGSTR3BData);
     }
   }, [gstin]);
   return (
@@ -39,8 +46,14 @@ export default function GSTSummary() {
       {/* <button onClick={() => file.print()}>Print PDF</button>
       <button onClick={() => file.download()}>Download PDF</button>
       <button onClick={() => file.open()}>Open PDF</button> */}
-      {GSTR9Data ? <DynamicGSTR9 tableData={GSTR9Data} /> : ""}
+      {/* {GSTR9Data ? <DynamicGSTR9 tableData={GSTR9Data} /> : ""} */}
       {/* <GSTR9 /> */}
+
+      {/* <button onClick={() => file.print()}>Print PDF</button>
+      <button onClick={() => file.download()}>Download PDF</button>
+      <button onClick={() => file.open()}>Open PDF</button> */}
+      {GSTR3BData ? <DynamicGSTR3B tableData={GSTR3BData} /> : ""}
+      {/* <GSTR3B /> */}
     </section>
   );
 }
