dccee41648e1097b2451b0c1c525f85975f7afc2
abhinay <abhinay@saaspay.app>
2022-12-15 13:05:20 +0530

    report generation

--- pages/api/report.js
+++ pages/api/report.js
@@ -33,10 +33,10 @@ export default async function handler(req, res) {
   let R3Data = processData(
     data.data,
     (key) =>
-      key == "itc_elg" ||
+      key === "itc_elg" ||
       key === "intr_ltfee" ||
-      key == "qn" ||
-      key == "sup_details"
+      key === "qn" ||
+      key === "sup_details"
   );
 
   // R9 Data
@@ -44,12 +44,35 @@ export default async function handler(req, res) {
   data = await response.json();
   let R9Data = processData(
     data.data,
-    (key) => key.startsWith("table") || key == "tax_pay"
+    (key) => key.startsWith("table") || key === "tax_pay"
   );
 
-  // TODO: DataSelection
+  let Report={
+    table1:new function(){
+      this.row1=new function (){
+        const data=R1Data.table12;
+        this.iamt=data.iamt
+        this.camt=data.camt
+        this.samt=data.samt
+        this.total=this.camt+this.iamt+this.samt
+      }
+      this.row2=new function (){
+        const data=R9Data.table4?.sup_adv;
+        this.iamt=data.iamt
+        this.camt=data.camt
+        this.samt=data.samt
+        this.total=this.camt+this.iamt+this.samt
+      }
+      this.row3={
+        iamt:this.row1.iamt-this.row2.iamt,
+        camt:this.row1.camt-this.row2.camt,
+        samt:this.row1.samt-this.row2.samt,
+        total:this.row1.total-this.row2.total
+      }
+    }
+  }
 
   console.log({ R1Data, R3Data, R9Data });
 
-  res.status(200).json({ success: true, data: { R1Data, R3Data, R9Data } });
+  res.status(200).json({ success: true, data: { R1Data, R3Data, R9Data, Report } });
 }
