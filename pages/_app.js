6815ac81552836d4bc11d133e2e2a15553c76716
delClg <lci2021011@iiitl.ac.in>
2023-03-11 08:43:33 +0530

    Added global App Context

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,32 @@
 import "../styles/globals.css";
 import "semantic-ui-css/semantic.min.css";
+import AppProvider from "../contexts/appContext.js";
+import { useState } from "react";
+import AppContext from "../contexts/appContext.js";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  let [appData, setAppData] = useState({ GSTINList: [], empDetails: null });
+  console.log("RERENDER", appData);
+  function updateGSTINList(list) {
+    console.log("LIST AT _APP", list);
+    setAppData({ ...appData, GSTINList: list });
+  }
+  function updateEmpDetails(empDetails) {
+    setAppData({ ...appData, empDetails: empDetails });
+  }
+  function update(value) {
+    setAppData({ ...appData, ...value });
+  }
+  return (
+    <AppContext.Provider
+      value={{
+        appData,
+        updates: { updateGSTINList, updateEmpDetails, update },
+      }}
+    >
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
 }
 
 export default MyApp;
