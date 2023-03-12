666a7a7b9ce8561cc0348b76f438bf8d9a7cc76c
delta68 <flyingdelta06082004@gmail.com>
2023-03-12 16:06:24 +0530

    Added Basic Styled Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,26 @@
-import styles from "../styles/Navbar.module.css";
-import { Image } from "semantic-ui-react";
+import { Image, Header } from "semantic-ui-react";
 
 function Navbar() {
   return (
-    <div className={styles.nav_container}>
-      <Image src="/income-tax-logo.png" size="medium" alt="Logo Image" />
-      <a className={styles.a} href="#">
-        Home
-      </a>
-    </div>
+    <nav className="nav">
+      <div></div>
+      <div className="nav_container">
+        <Image
+          src="https://comtax.up.nic.in/GSThome/sites/default/files/logo.png"
+          alt="Logo"
+        />
+        <div className="nav_main">
+          <Header as="h1" className="heading">
+            Department of Commercial Tax <br />
+            <span className="ui header subheading">Uttar-Pradesh</span>
+          </Header>
+        </div>
+        <Image
+          src="https://comtax.up.nic.in/GSThome/sites/default/files/upgov.png"
+          alt="Govt. of UP"
+        />
+      </div>
+    </nav>
   );
 }
 
