2fbafb330b56ee611e867e36213305d983ee00c9
delta68 <flyingdelta06082004@gmail.com>
2023-03-20 23:34:14 +0530

    renamed Add remarks to upload remarks

--- pages/report/[gstin].js
+++ pages/report/[gstin].js
@@ -337,7 +337,7 @@ export default function GSTSummary() {
                       fluid
                       disabled={pdfUrl == null}
                     >
-                      Add Remarks
+                      Upload Remarks
                     </Button>
                   }
                 >
