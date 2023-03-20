bd0261c8b6f4ff61ac0b9e7bd63675b4c91567ca
delta68 <flyingdelta06082004@gmail.com>
2023-03-20 23:25:10 +0530

    gstin table added color legend

--- components/GSTINTable.js
+++ components/GSTINTable.js
@@ -1,5 +1,12 @@
 import React, { useContext } from "react";
-import { Button, Table } from "semantic-ui-react";
+import {
+  Button,
+  Label,
+  Message,
+  Popup,
+  Segment,
+  Table,
+} from "semantic-ui-react";
 import { AppContext } from "../contexts/appContext";
 import { purgeText } from "./utils";
 
@@ -7,75 +14,125 @@ export function RawGSTINTable({ onRowClick, onReportBtnClick }) {
   const appContext = useContext(AppContext);
 
   return (
-    <Table celled selectable padded>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell textAlign="center">S. No.</Table.HeaderCell>
-          <Table.HeaderCell textAlign="center">GSTIN Number</Table.HeaderCell>
-          <Table.HeaderCell textAlign="center">Trade Name</Table.HeaderCell>
-          <Table.HeaderCell textAlign="center">Legal Name</Table.HeaderCell>
-          <Table.HeaderCell textAlign="center">
-            Generate Notice
-          </Table.HeaderCell>
-          <Table.HeaderCell textAlign="center">Remarks</Table.HeaderCell>
-          <Table.HeaderCell textAlign="center">
+    <Segment>
+      <Message floating color="teal" compact>
+        <Message.Header>Color Codes</Message.Header>
+        <Message.List>
+          <Message.Item>
+            <Popup
+              content="Green"
+              trigger={
+                <Label circular color="green">
+                  G
+                </Label>
+              }
+            />{" "}
+            No Action Required
+          </Message.Item>
+          <Message.Item>
+            <Popup
+              content="Blue"
+              trigger={
+                <Label circular color="blue">
+                  B
+                </Label>
+              }
+            />{" "}
+            Visited
+          </Message.Item>
+          <Message.Item>
+            <Popup
+              content="Red"
+              trigger={
+                <Label circular color="red">
+                  R
+                </Label>
+              }
+            />{" "}
             Action Required
-          </Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {appContext.appData.GSTINList.map(
-          (
-            { id, legal_name, trade_name, gstin, actionRequired, review },
-            ind
-          ) => {
-            return (
-              <Table.Row
-                key={gstin}
-                positive={actionRequired != null && !actionRequired}
-                negative={actionRequired != null && actionRequired}
-              >
-                <Table.Cell textAlign="center">{ind + 1}</Table.Cell>
-                <Table.Cell
-                  className="is-hoverable"
-                  selectable
-                  textAlign="center"
-                  onClick={() =>
-                    onRowClick({
-                      id,
-                      legal_name,
-                      trade_name,
-                      gstin,
-                      actionRequired,
-                      review,
-                    })
-                  }
+          </Message.Item>
+        </Message.List>
+      </Message>
+      <Table celled selectable padded>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell textAlign="center">S. No.</Table.HeaderCell>
+            <Table.HeaderCell textAlign="center">GSTIN Number</Table.HeaderCell>
+            <Table.HeaderCell textAlign="center">Trade Name</Table.HeaderCell>
+            <Table.HeaderCell textAlign="center">Legal Name</Table.HeaderCell>
+            <Table.HeaderCell textAlign="center">
+              Generate Notice
+            </Table.HeaderCell>
+            <Table.HeaderCell textAlign="center">Remarks</Table.HeaderCell>
+            <Table.HeaderCell textAlign="center">
+              Action Required
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {appContext.appData.GSTINList.map(
+            (
+              {
+                id,
+                legal_name,
+                trade_name,
+                gstin,
+                actionRequired,
+                review,
+                viewed,
+              },
+              ind
+            ) => {
+              return (
+                <Table.Row
+                  key={gstin}
+                  positive={actionRequired != null && !actionRequired}
+                  negative={actionRequired != null && actionRequired}
+                  className={actionRequired == null && viewed ? "bg-blue" : ""}
                 >
-                  {gstin}
-                </Table.Cell>
-                <Table.Cell textAlign="left">{trade_name || "-"}</Table.Cell>
-                <Table.Cell textAlign="left">{legal_name || "-"}</Table.Cell>
-                <Table.Cell textAlign="center">
-                  <Button onClick={() => onReportBtnClick(gstin)}>
-                    Generate Report
-                  </Button>
-                </Table.Cell>
-                <Table.Cell textAlign="center">
-                  {review ? purgeText(review) : ""}
-                </Table.Cell>
-                <Table.Cell textAlign="center">
-                  {actionRequired == null
-                    ? ""
-                    : actionRequired
-                    ? "True"
-                    : "False"}
-                </Table.Cell>
-              </Table.Row>
-            );
-          }
-        )}
-      </Table.Body>
-    </Table>
+                  <Table.Cell textAlign="center">{ind + 1}</Table.Cell>
+                  <Table.Cell
+                    className="is-hoverable"
+                    selectable
+                    textAlign="center"
+                    onClick={() =>
+                      onRowClick({
+                        id,
+                        legal_name,
+                        trade_name,
+                        gstin,
+                        actionRequired,
+                        review,
+                        viewed,
+                      })
+                    }
+                  >
+                    {gstin}
+                  </Table.Cell>
+                  <Table.Cell textAlign="left">{trade_name || "-"}</Table.Cell>
+                  <Table.Cell textAlign="left">{legal_name || "-"}</Table.Cell>
+                  <Table.Cell textAlign="center">
+                    <Button onClick={() => onReportBtnClick(gstin)}>
+                      Generate Report
+                    </Button>
+                  </Table.Cell>
+                  <Table.Cell textAlign="center">
+                    {review ? purgeText(review) : ""}
+                  </Table.Cell>
+                  <Table.Cell textAlign="center">
+                    {actionRequired == null
+                      ? ""
+                      : actionRequired
+                      ? "True"
+                      : "False"}
+                  </Table.Cell>
+                </Table.Row>
+              );
+            }
+          )}
+        </Table.Body>
+      </Table>
+    </Segment>
   );
 }
 
