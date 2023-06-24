d3c14405afd52924e90e102753b5ad62dae02b00
delta68 <flyingdelta06082004@gmail.com>
2023-06-25 00:38:24 +0530

    updated entry form to handle empty strings

--- components/EntryForm.js
+++ components/EntryForm.js
@@ -16,14 +16,51 @@ export default function EntryForm({
   open,
   onOpenStateChange,
 }) {
-  let [formData, setFormData] = useState({
+  const [formData, setFormData] = useState({
     boweb: 0,
     drc: 0,
     further_action: 0,
     gstin_array: [],
   });
-  let [GSTINInp, setGSTINInp] = useState("");
-  let [loading, setLoading] = useState(false);
+  const [GSTINInp, setGSTINInp] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleInputChange = (field, value) => {
+    const parsedValue = value === "" ? "" : parseInt(value);
+    setFormData((prevData) => ({ ...prevData, [field]: parsedValue }));
+  };
+
+  const handleAddGSTIN = () => {
+    if (!checkGSTIN(GSTINInp)) return;
+
+    const trimmedGSTIN = GSTINInp.trim();
+    if (formData.gstin_array.includes(trimmedGSTIN)) return;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      gstin_array: [...prevData.gstin_array, trimmedGSTIN],
+    }));
+    setGSTINInp("");
+  };
+
+  const handleRemoveGSTIN = (gstin) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      gstin_array: prevData.gstin_array.filter((i_gstin) => i_gstin !== gstin),
+    }));
+  };
+
+  const handleFormSubmit = async () => {
+    setLoading(true);
+    await handleOnSubmit(formData);
+    setFormData({
+      boweb: 0,
+      drc: 0,
+      further_action: 0,
+      gstin_array: [],
+    });
+    setLoading(false);
+  };
 
   return (
     <Modal
@@ -42,50 +79,39 @@ export default function EntryForm({
               <Form.Input
                 type="number"
                 value={formData.boweb}
-                onChange={(e, { value }) =>
-                  setFormData({ ...formData, boweb: parseInt(value) })
-                }
+                onChange={(e, { value }) => handleInputChange("boweb", value)}
               />
             </Form.Group>
-            <Form.Group inline widths={"equal"}>
+            <Form.Group inline widths="equal">
               <label>
                 Amount Deposited After Issuing of ASMT-10 through (DRC-03)
               </label>
               <Form.Input
                 type="number"
                 value={formData.drc}
-                onChange={(e, { value }) =>
-                  setFormData({ ...formData, drc: parseInt(value) })
-                }
+                onChange={(e, { value }) => handleInputChange("drc", value)}
               />
             </Form.Group>
-            <Form.Group inline widths={"equal"}>
+            <Form.Group inline widths="equal">
               <label>No of Dealer Selected For further Action for 73/74</label>
               <Form.Input
                 type="number"
                 value={formData.further_action}
                 onChange={(e, { value }) =>
-                  setFormData({ ...formData, further_action: parseInt(value) })
+                  handleInputChange("further_action", value)
                 }
               />
             </Form.Group>
-            <Form.Group inline widths={"equal"}>
+            <Form.Group inline widths="equal">
               <label>Add GSTIN</label>
               <Form.Input
                 type="text"
-                error={GSTINInp != "" ? !checkGSTIN(GSTINInp) : false}
+                error={GSTINInp !== "" && !checkGSTIN(GSTINInp)}
                 action={
                   <Button
                     icon="add"
                     disabled={!checkGSTIN(GSTINInp)}
-                    onClick={() => {
-                      if (formData.gstin_array.includes(GSTINInp)) return;
-                      setFormData({
-                        ...formData,
-                        gstin_array: [...formData.gstin_array, GSTINInp.trim()],
-                      });
-                      setGSTINInp("");
-                    }}
+                    onClick={handleAddGSTIN}
                   />
                 }
                 value={GSTINInp}
@@ -102,14 +128,7 @@ export default function EntryForm({
                   <Icon
                     name="delete"
                     link
-                    onClick={() => {
-                      setFormData({
-                        ...formData,
-                        gstin_array: formData.gstin_array.filter(
-                          (i_gstin) => i_gstin != gstin
-                        ),
-                      });
-                    }}
+                    onClick={() => handleRemoveGSTIN(gstin)}
                   />
                 </Label>
               ))}
@@ -130,23 +149,13 @@ export default function EntryForm({
         <Button
           loading={loading}
           disabled={
-            formData.boweb != formData.gstin_array.length ||
-            (formData.further_action == 0 && formData.drc == 0)
+            formData.boweb !== formData.gstin_array.length ||
+            (!formData.boweb && !formData.further_action && !formData.drc)
           }
           content="Save"
           labelPosition="right"
           icon="checkmark"
-          onClick={async () => {
-            setLoading(true);
-            await handleOnSubmit(formData);
-            setFormData({
-              boweb: 0,
-              drc: 0,
-              further_action: 0,
-              gstin_array: [],
-            });
-            setLoading(false);
-          }}
+          onClick={handleFormSubmit}
           positive
         />
       </Modal.Actions>
